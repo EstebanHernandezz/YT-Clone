d47f4f05da59c435745ba8f9daf2d16efb0c7a9a
Esteban Hernandez <69884185+EstebanHernandezz@users.noreply.github.com>
2024-05-22 13:09:58 -0500

    Update index.ts

--- ty-api-service/index.ts
+++ ty-api-service/index.ts
@@ -2,10 +2,15 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from"@google-cloud/storage";
+import { onCall } from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucket = "";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
